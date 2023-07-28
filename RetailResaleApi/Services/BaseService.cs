72248ce2b6fe7394b8c5a77d8ca28c71378fb640
Lauren <laurenmiles.dev@gmail.com>
2023-07-28 10:02:30 -0400

    Created BaseService

---
+++ RetailResaleApi/Services/BaseService.cs
@@ -0,0 +1,11 @@
+﻿using RetailResaleApi.Context;
+
+namespace RetailResaleApi.Services
+{
+    public abstract class BaseService
+    {
+        protected readonly RetailResaleContext DbContext;
+
+        protected BaseService(RetailResaleContext dbContext) { DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext)); }
+    }
+}
