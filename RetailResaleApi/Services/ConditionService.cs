dba4ee7ffb16fef704432f480846c8b4246cfb37
Lauren <laurenmiles.dev@gmail.com>
2023-09-26 09:52:16 -0400

    Created ConditionsService

---
+++ RetailResaleApi/Services/ConditionService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailResaleApi.Context;
+using RetailResaleApi.Data.Entities;
+using RetailResaleApi.Interfaces;
+
+namespace RetailResaleApi.Services
+{
+    public class ConditionService : BaseService, IConditionService
+    {
+        public ConditionService(RetailResaleContext dbContext) : base(dbContext) { }
+
+        public async Task<Condition?> GetConditionById(int id)
+        {
+            var condition = await DbContext.Set<Condition>()
+                .FirstOrDefaultAsync(ctx => ctx.Id == id);
+
+            return condition;
+        }
+
+        public async Task<IEnumerable<Condition>> GetConditions()
+        {
+            var conditions = await DbContext.Set<Condition>()
+                .ToListAsync();
+
+            return conditions;
+        }
+    }
+}
