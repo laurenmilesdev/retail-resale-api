cbeb4e01bd79066a01f69b65dcd3e29b847f931e
Lauren <laurenmiles.dev@gmail.com>
2023-09-26 09:52:36 -0400

    Added service dependency

--- RetailResaleApi/Program.cs
+++ RetailResaleApi/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<RetailResaleContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("RetailResaleDb")));
 
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IConditionService, ConditionService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
