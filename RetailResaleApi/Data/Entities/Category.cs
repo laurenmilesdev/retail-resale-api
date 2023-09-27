a4da6194ef57cc39cf1338ad34e9e2ecf2629392
Lauren <laurenmiles.dev@gmail.com>
2023-09-27 10:56:57 -0400

    Everything else

--- RetailResaleApi/Data/Entities/Category.cs
+++ RetailResaleApi/Data/Entities/Category.cs
@@ -3,5 +3,6 @@
     public class Category : BaseEntity
     {
         public string Value { get; set; } = null!;
+        public ICollection<SubCategory> SubCategories { get; set; } = new List<SubCategory>();
     }
 }
