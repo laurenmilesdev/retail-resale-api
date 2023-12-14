71962764e9b247010ce62ff8e9e96535ac02d356
Lauren Miles <133659063+laurenmilesdev@users.noreply.github.com>
2023-12-14 10:49:15 -0500

    Updated relationship between 2 tables (#7)

---
+++ RetailResaleApi/Migrations/20231214154335_UpdateSoldListingSiteProductRelationship.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RetailResaleApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSoldListingSiteProductRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sold_ListingSiteProduct_ListingSiteProductId",
+                table: "Sold");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sold_ListingSiteProductId",
+                table: "Sold");
+
+            migrationBuilder.DropColumn(
+                name: "ListingSiteProductId",
+                table: "Sold");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SoldId",
+                table: "ListingSiteProduct",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ListingSiteProduct_SoldId",
+                table: "ListingSiteProduct",
+                column: "SoldId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ListingSiteProduct_Sold_SoldId",
+                table: "ListingSiteProduct",
+                column: "SoldId",
+                principalTable: "Sold",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ListingSiteProduct_Sold_SoldId",
+                table: "ListingSiteProduct");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ListingSiteProduct_SoldId",
+                table: "ListingSiteProduct");
+
+            migrationBuilder.DropColumn(
+                name: "SoldId",
+                table: "ListingSiteProduct");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ListingSiteProductId",
+                table: "Sold",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sold_ListingSiteProductId",
+                table: "Sold",
+                column: "ListingSiteProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sold_ListingSiteProduct_ListingSiteProductId",
+                table: "Sold",
+                column: "ListingSiteProductId",
+                principalTable: "ListingSiteProduct",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
